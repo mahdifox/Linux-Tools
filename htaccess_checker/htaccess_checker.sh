19dc81a5a616de261a84c2e38c5ce4084d616198
Mahdi Bagheri <mahdifox2000@gmail.com>
2020-07-11 00:41:10 +0430

    all subdomains added

--- htaccess_checker/htaccess_checker.sh
+++ htaccess_checker/htaccess_checker.sh
@@ -2,14 +2,23 @@
 
 ##########################################
 ## Created_By_Mahdi_Bagheri_at_2020_Jun ##
-##     htaccess checker for prevent     ##
-##         misuse of resources          ##
+##	 htaccess checker for prevent   ##
+##		 misuse of resources    ##
 ##########################################
 
 ################# Usage ##################
 ##  run script from server's cronetab   ##
-##    as root, no arguments needed      ##
+##	as root, no arguments needed    ##
 ##########################################
+
+#MY_PATH filled with the executinve script path
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
 
 #define color for print or echo
 GREEN='\033[0;32m'
@@ -20,50 +29,50 @@ NC='\033[0m'
 LOG_PATH="/var/log/htaccess_checker";
 mkdir -p $LOG_PATH;
 
-
-# declare an array to get user-id and home-directory
-# of all linux users according to /etc/passwd file named p.
+# declare p array to get user-id and home-directory of all linux users according to /etc/passwd file.
 declare -A p=()
 while IFS=: read -r x x uid x x home x
 do
-    p[$uid]=$home
+	p[$uid]=$home
 done </etc/passwd
 
+# checking which users are usual users.
+user_id_min=`cat /etc/login.defs | grep "^UID_MIN" | awk '{print $2}'`
+user_id_max=`cat /etc/login.defs | grep "^UID_MAX" | awk '{print $2}'`
 
-# using of array p.
-for uid in "${!p[@]}"
+# checking every single php_value
+while read php_value
 do
-
-    # checking which users are usual users.
-    user_id_min=`cat /etc/login.defs | grep "^UID_MIN" | awk '{print $2}'`
-    user_id_max=`cat /etc/login.defs | grep "^UID_MAX" | awk '{print $2}'`
-    if [ $uid -ge $user_id_min ] && [ $uid -le $user_id_max ]
-    then
-
-	# checking if .htaccess file exists in the user's home directory or not.
-	if [ -f "${p[$uid]}/public_html/.htaccess" ]
-	then
-	    
-	    # checking a foreign file named php_value for have a list of illegal php parameters that must not use in user's htaccess.
-	    # !IMPORTANT: the file must be in the same path of this script file.
-	    while read value
-	    do
-		
-		#checking if every single php value from the foreign file exists in htaccess file of user's home-directory or not.
-	   	if grep -q $value "${p[$uid]}/public_html/.htaccess";
-	   	then
-		   	HT_PATH="${p[$uid]}/public_html/.htaccess";
-		   	USERNAME=`echo "${p[$uid]}" |cut -d\/ -f3`;
-		   	#comment the line including illegal php value.
-			sed -e '/'"$value"'/ s/^#*/#/' -i "$HT_PATH";
-		   	#setting the privilege of htaccess to root and read-only.
-			chown  root.root "$HT_PATH";
-		   	chmod  400    	 "$HT_PATH";
-			#printing the event log in the log file defined at the top of this script.
-			now_date=`date "+%T %Y-%m-%d"`;
-		   	echo -e "The user ${RED}$USERNAME${NC} has been blocked, illegal using of ${RED}$value${NC} value in ${RED}${p[$uid]}/public_html/.htaccess${NC} file at ${GREEN}$now_date${NC}\n" >> "$LOG_PATH/blocked";
-	   	fi
-	    done <./php_values
-	fi
-    fi
-done
+	#  using p array.
+	for uid in "${!p[@]}"
+	do
+		if [ $uid -ge $user_id_min ] && [ $uid -le $user_id_max ]
+		then
+			# geting exact username in USERNAME variable.
+			USERNAME=`echo "${p[$uid]}" |cut -d\/ -f3`;
+			# checking if username is one of clients.
+			if [ $USERNAME != "mahdi" ]
+			then
+				# checking if .htaccess file exists in user's public_html or sub-directories and store them to .all_htaccess under user's home-directory.
+				grep -rn "$php_value" ${p[$uid]} | grep ".htaccess" | cut -d':' -f1 >> "${p[$uid]}/.all_htaccess";
+				# set admin-access permission for .htaccess file.
+				chown	root.root	"${p[$uid]}/.all_htaccess";
+				chmod	600		"${p[$uid]}/.all_htaccess";
+				while read ht_path
+				do
+					# comment illegal php_value in ht_path .htaccess file.
+					sed -e '/^#/! {/'"$php_value"'/ s/^/#/}' -i "$ht_path";
+					# set admin-access permission for .htaccess file.
+					chown	root.root	"$ht_path";
+					chmod	600		"$ht_path";
+					# geting now date for log exact time
+					now_date=`date "+%T %Y-%m-%d"`;
+					# printing  event log in the log file defined at the top of script ( $LOG_PATH/blocked ).
+					echo -e "The user ${RED}$USERNAME${NC} has been blocked, illegal using of ${RED}$php_value${NC} value in ${RED}$ht_path${NC} file at ${GREEN}$now_date${NC}\n" >> "$LOG_PATH/blocked";
+				done <"${p[$uid]}/.all_htaccess"
+				rm -f "${p[$uid]}/.all_htaccess";
+			fi
+		fi
+	done
+done < "$MY_PATH/php_values"
+# everything is done now...

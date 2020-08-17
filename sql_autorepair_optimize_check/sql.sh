b78ae89d1f42881e00661d3b41461cf0ca5ea151
Mahdi Bagheri <mahdifox2000@gmail.com>
2020-08-17 17:41:52 +0430

    update sql.sh
    all cpanel and directadmin  supported now. :)

--- sql_autorepair_optimize_check/sql.sh
+++ sql_autorepair_optimize_check/sql.sh
@@ -2,20 +2,20 @@
 
 ##########################################
 ## Created_By_Mahdi_Bagheri_at_2020_Aug ##
-##	  check optimize autorepair	##
-##	        in mysql		##
+##        check optimize autorepair     ##
+##              in mysql                ##
 ##########################################
 
 ################# Usage ##################
-##	  run script without args	##
-##	      to see all db		##
-##					##
-##	  all or db argument needed	##
-##		to run correctly        ##
+##        run script without args       ##
+##            to see all db             ##
+##                                      ##
+##        all or db argument needed     ##
+##              to run correctly        ##
 ##########################################
 
 
-MY_PATH="`dirname \"$0\"`"			  # relative
+MY_PATH="`dirname \"$0\"`"                        # relative
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 if [ -z "$MY_PATH" ] ; then
   # error; for some reason, the path is not accessible
@@ -30,58 +30,102 @@ GREEN='\033[0;32m';
 RED='\033[0;31m';
 NC='\033[0m'
 
-dbuser=`cat /usr/local/directadmin/conf/my.cnf  | grep user | cut -d'=' -f2`;
-dbpass=`cat /usr/local/directadmin/conf/my.cnf  | grep password | cut -d'=' -f2`;
-mysql -u$dbuser -p$dbpass -e 'show databases;' | grep -v "|" | grep -v "Database\|information_schema\|performance_schema\|mysql" > $MY_PATH/all_dbs;
+if [ -d "/opt/cpanel" ]
+then
+        dbuser="cpanel";
+        dbpass="cpanel";
+        mysql -e 'show databases;' | grep -v "|" | grep -v "Database\|information_schema\|performance_schema\|mysql" > $MY_PATH/all_dbs;
+
+elif [ -d "/usr/local/directadmin" ]
+then
+        dbuser=`cat /usr/local/directadmin/conf/my.cnf  | grep user | cut -d'=' -f2`;
+        dbpass=`cat /usr/local/directadmin/conf/my.cnf  | grep password | cut -d'=' -f2`;
+        mysql -u$dbuser -p$dbpass -e 'show databases;' | grep -v "|" | grep -v "Database\|information_schema\|performance_schema\|mysql" > $MY_PATH/all_dbs;
+
+else
+        echo -e "${RED} failed: No cpanel or directadmin Panel detected ${NC}";
+        exit 1  # fail
+fi
 
 update=$1;
 if [[ ! -z $update ]]
 then
-		logpath="/var/www/html/SQL_script";
-		mkdir -p  "$logpath";
+        logpath="/var/www/html/SQL_script";
+        mkdir -p  "$logpath";
 
-	if [ $update == "all" ]
-	then
-		echo -e "${GREEN} waiting for fetching all databases ... ${NC}";
-		echo -e "${GREEN} done ${NC}";
-		while read line
-		do
-			mysqlcheck -u$dbuser -p$dbpass --auto-repair $line && mysqlcheck -u$dbuser -p$dbpass --optimize $line && mysqlcheck -u$dbuser -p$dbpass --check $line
-			if [ $? -eq 0 ]
-				then
-					mkdir -p "$logpath/`date "+%Y-%m-%d"`";
-					now_time=`date "+%T"`;
-					echo -e "auto-repair, optimize and check ${GREEN}done${NC} for ${GREEN}$line${NC} database at ${BOLD}$now_time${NORMAL}" > "$logpath/`date "+%Y-%m-%d"`/sql_script.log";
-				else
-					mkdir -p "$logpath/`date "+%Y-%m-%d"`";
-										now_time=`date "+%T"`;
-					echo -e "auto-repair, optimize and check ${RED}failed${NC} for ${RED}$line${NC} database at ${BOLD}$now_time${NORMAL}" > "$logpath/`date "+%Y-%m-%d"`/sql_script.err.log";
-			fi
-		done<$MY_PATH/all_dbs;
-	else
-		while read line2
-		do
-			if [ $update == $line2 ]
-				mysqlcheck -u$dbuser -p$dbpass --auto-repair $line2 && mysqlcheck -u$dbuser -p$dbpass --optimize $line2 && mysqlcheck -u$dbuser -p$dbpass --check $line2
-				if [ $? -eq 0 ]
-				then
-					mkdir -p "$logpath/`date "+%Y-%m-%d"`";
-					now_time=`date "+%T"`;
-					echo -e "auto-repair, optimize and check ${GREEN}done${NC} for ${GREEN}$line${NC} database at ${BOLD}$now_time${NORMAL}" > "$logpath/`date "+%Y-%m-%d"`/sql_script.log";
-				else
-					mkdir -p "$logpath/`date "+%Y-%m-%d"`";
-					now_time=`date "+%T"`;
-					echo -e "auto-repair, optimize and check ${RED}failed${NC} for ${RED}$line${NC} database at ${BOLD}$now_time${NORMAL}" > "$logpath/`date "+%Y-%m-%d"`/sql_script.err.log";
-				fi
-			fi
-		done<$MY_PATH/all_dbs;
-	fi
+        if [ $update == "all" ]
+        then
+                echo -e "${GREEN} waiting for fetching all databases ... ${NC}";
+                echo -e "${GREEN} done ${NC}";
+                while read line
+                do
+                        if [ $dbuser == "cpanel" ] && [ $dbpass == "cpanel" ]
+                        then
+                                mysqlcheck --auto-repair $line && mysqlcheck --optimize $line && mysqlcheck --check $line;
+                                if [ $? -eq 0 ]
+                                then
+                                        mkdir -p "$logpath/`date "+%Y-%m-%d"`";
+                                        now_time=`date "+%T"`;
+                                        echo -e "auto-repair, optimize and check ${GREEN}done${NC} for ${GREEN}$line${NC} database at ${BOLD}$now_time${NORMAL}" > "$logpath/`date "+%Y-%m-%d"`/sql_script.log";
+                                else
+                                        mkdir -p "$logpath/`date "+%Y-%m-%d"`";
+                                        now_time=`date "+%T"`;
+                                        echo -e "auto-repair, optimize and check ${RED}failed${NC} for ${RED}$line${NC} database at ${BOLD}$now_time${NORMAL}" > "$logpath/`date "+%Y-%m-%d"`/sql_script.err.log";
+                                fi
+
+                        else
+                                mysqlcheck -u$dbuser -p$dbpass --auto-repair $line && mysqlcheck -u$dbuser -p$dbpass --optimize $line && mysqlcheck -u$dbuser -p$dbpass --check $line
+                                if [ $? -eq 0 ]
+                                then
+                                        mkdir -p "$logpath/`date "+%Y-%m-%d"`";
+                                        now_time=`date "+%T"`;
+                                        echo -e "auto-repair, optimize and check ${GREEN}done${NC} for ${GREEN}$line${NC} database at ${BOLD}$now_time${NORMAL}" > "$logpath/`date "+%Y-%m-%d"`/sql_script.log";
+                                else
+                                        mkdir -p "$logpath/`date "+%Y-%m-%d"`";
+                                                                                now_time=`date "+%T"`;
+                                        echo -e "auto-repair, optimize and check ${RED}failed${NC} for ${RED}$line${NC} database at ${BOLD}$now_time${NORMAL}" > "$logpath/`date "+%Y-%m-%d"`/sql_script.err.log";
+                                fi
+                        fi
+                done<$MY_PATH/all_dbs;
+        else
+                while read line2
+                do
+                        if [ $update == $line2 ]
+                                if [ $dbuser == "cpanel" ] && [ $dbpass == "cpanel" ]
+                                then
+                                        mysqlcheck --auto-repair $line2 && mysqlcheck --optimize $line2 && mysqlcheck --check $line2
+                                        if [ $? -eq 0 ]
+                                        then
+                                                mkdir -p "$logpath/`date "+%Y-%m-%d"`";
+                                                now_time=`date "+%T"`;
+                                                echo -e "auto-repair, optimize and check ${GREEN}done${NC} for ${GREEN}$line${NC} database at ${BOLD}$now_time${NORMAL}" > "$logpath/`date "+%Y-%m-%d"`/sql_script.log";
+                                        else
+                                                mkdir -p "$logpath/`date "+%Y-%m-%d"`";
+                                                now_time=`date "+%T"`;
+                                                echo -e "auto-repair, optimize and check ${RED}failed${NC} for ${RED}$line${NC} database at ${BOLD}$now_time${NORMAL}" > "$logpath/`date "+%Y-%m-%d"`/sql_script.err.log";
+                                        fi
+                                else
+                                        mysqlcheck -u$dbuser -p$dbpass --auto-repair $line2 && mysqlcheck -u$dbuser -p$dbpass --optimize $line2 && mysqlcheck -u$dbuser -p$dbpass --check $line2
+                                        if [ $? -eq 0 ]
+                                        then
+                                                mkdir -p "$logpath/`date "+%Y-%m-%d"`";
+                                                now_time=`date "+%T"`;
+                                                echo -e "auto-repair, optimize and check ${GREEN}done${NC} for ${GREEN}$line${NC} database at ${BOLD}$now_time${NORMAL}" > "$logpath/`date "+%Y-%m-%d"`/sql_script.log";
+                                        else
+                                                mkdir -p "$logpath/`date "+%Y-%m-%d"`";
+                                                now_time=`date "+%T"`;
+                                                echo -e "auto-repair, optimize and check ${RED}failed${NC} for ${RED}$line${NC} database at ${BOLD}$now_time${NORMAL}" > "$logpath/`date "+%Y-%m-%d"`/sql_script.err.log";
+                                        fi
+                                fi
+                        fi
+                done<$MY_PATH/all_dbs;
+        fi
 else
-	echo -e "${GREEN} waiting for fetching all databases ... ${NC}";
-	echo -e "${GREEN} done ${NC}";	
-	while read line3
-	do
-			available_dbs="$line3 | $available_dbs";
-	done<$MY_PATH/all_dbs;
-	echo $"Usage: $0 { "$available_dbs"all }";
+        echo -e "${GREEN} waiting for fetching all databases ... ${NC}";
+        echo -e "${GREEN} done ${NC}";
+        while read line3
+        do
+                available_dbs="$line3 | $available_dbs";
+        done<$MY_PATH/all_dbs;
+        echo $"Usage: $0 { "$available_dbs"all }";
 fi

73d7c10c0e3b48e40a94dc5f2605bea78a9c8207
root <root@server.domain.com>
2020-08-19 08:57:27 +0430

    DirectAdmin_Check_UP is added

---
+++ directAdmin_Check_UP/script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+##########################################
+## Created_By_Mahdi_Bagheri_at_2020_Aug ##
+##           checking if DA up          ##
+##########################################
+
+################# Usage ##################
+##        run script without args       ##
+##          as root via cron            ##
+##########################################
+while true
+do
+	if ! nc -w 1 -z localhost 4041
+	then
+		service directadmin restart;
+		sleep 15;
+	else
+		# DirectAdmin is UP, so quit the script.
+		exit 1;
+	fi
+done

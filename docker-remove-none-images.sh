35659a3f1ed9c9d894971d4100b1472e37bfa8cd
root <root@git.ferdowsi.cloud>
2021-04-26 10:49:07 +0430

    docker-remove-none-images.sh added

---
+++ docker-remove-none-images.sh
@@ -0,0 +1,26 @@
+#bin/bash
+
+##########################################
+## Created_By_Mahdi_Bagheri_at_2021_Apr ##
+##     on host that must backed up      ##
+##########################################
+
+################# Usage ##################
+##        run script with script        ##
+##########################################
+
+GREEN='\033[0;32m';
+NC='\033[0m';
+
+count=`docker images | tail -n+2 | wc -l`;
+
+for ((i=1;i<=$count;i++))
+do
+	repo=`docker images | tail -n+2 | awk '{print $1}' | awk "NR==$i"`;
+	name=`docker images | tail -n+2 | awk "NR==$i"`;
+	if [ "$repo" == "<none>" ]
+	then
+		docker rmi $(docker images  | tail -n+2 | awk '{print $3}' | awk "NR==$i") --force > /dev/null;
+		echo -e "${GREEN} REMOVING ${NC} $name ${GREEN} :) ${NC}";
+	fi
+done
